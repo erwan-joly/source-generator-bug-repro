9bba6ceb574d05f47993c41200dca66f83a920d2
erwan-joly <erwan@onwardly.io>
2022-09-12 10:02:49 +1200

    small repro

---
+++ SourceGenerator/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+
+
+using System.Text.Json;
+namespace App
+{
+    [Generator]
+    public class HelloWorldGenerator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            string jsonString = JsonSerializer.Serialize("test");
+            // begin creating the source we'll inject into the users compilation
+            var sourceBuilder = new StringBuilder(@"
+using System;
+namespace HelloWorldGenerated
+{
+    public static class HelloWorld
+    {
+        public static void SayHello() 
+        {
+            Console.WriteLine(""Hello from generated code!"");
+        }
+    }
+}");
+
+            // inject the created source into the users compilation
+            context.AddSource(
+                "HelloWorld.generated.cs",
+                SourceText.From(sourceBuilder.ToString(), Encoding.UTF8)
+            );
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            // No initialization required for this one
+        }
+    }
+}
+
